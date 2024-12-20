f1671a382af72e5333b97b604a7d9a0136aa7456
Brayan Esteves <lila.brayan.steves@somosoh.pe>
2024-12-20 07:10:44 -0400

    (With template engine 'Smarty') 03 - Views and Layout [PHP version 5.1]

---
+++ version/5.1/00 - With template engine 'Smarty'/03.views-and-layout/controllers/indexController.php
@@ -0,0 +1,15 @@
+<?php
+    class indexController extends Controller {
+
+        public function __construct() {
+            parent::__construct();
+        }
+
+        public function index() {
+            // echo 'Hola desde el indexController... ';
+            $this->_view->titulo = 'Portada';
+            // $this->_view->renderizar('index');
+            $this->_view->renderizar('index', 'inicio');
+        }
+    }
+?>
