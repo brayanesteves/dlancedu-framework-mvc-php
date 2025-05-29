1df8668ecdd28fc990d8237f47ffaef5a3cfce31
Brayan Esteves <brayan.esteves93@gmail.com>
2025-05-29 10:24:41 -0400

    (With template engine 'Smarty') 07 - Session & Access Control [PHP version 5.1] [29/05/2025]

---
+++ version/5.1/00 - With template engine 'Smarty'/07.sessions-and-access-control/controllers/loginController.php
@@ -0,0 +1,28 @@
+<?php
+    class loginController extends Controller {
+        public function __construct() {
+            parent::__construct();
+        }
+
+        public function index() {
+            Session::set('autenticado', true);
+            Session::set('level', 'usuario');
+
+            Session::set('var1', 'var1');
+            Session::set('var2', 'var2');
+            
+            $this->redireccionar('login/mostrar');
+        }
+
+        public function mostrar() {
+            echo 'Level: ' . Session::get('level') . '<br>';
+            echo 'Var1: ' . Session::get('var1') . '<br>';
+            echo 'Var2: ' . Session::get('var2') . '<br>';
+        }
+
+        public function cerrar() {
+            Session::destroy(array('var1', 'var2'));
+            $this->redireccionar('login/mostrar');
+        }
+    }
+?>
---
+++ version/5.1/00 - With template engine 'Smarty'/07.sessions-and-access-control/views/layout/layout1/header.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html lang="en">
+<head>
+    <title><?php if(isset($this->titulo)) echo $this->titulo; ?></title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf8" />
+    <link href="<?php echo $_layoutParams['ruta_css']; ?>style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+    <div id="dvmaincontainer">
+    <!--main div container starts here-->
+
+        <div id="dvtopcontainer">
+        <!--top div container starts here-->
+            <div id="dvlogocontainer">
+            <!--logo div container starts here-->
+
+                <h1><?php echo APP_NAME; ?></h1>
+                <h1><?php echo APP_SLOGAN; ?></h1>
+            <!--logo div container end here-->
+            </div>
+
+            <div id="dvnavicontainer">
+            <!--navigation div container starts here-->
+                <img src="<?php echo $_layoutParams['ruta_img'] ?>navi_left.jpg" alt="navi_left" />
+                <div id="tabs1">
+                    <ul>
+                    <?php if(isset($_layoutParams['menu'])): ?>
+                    <?php for($i = 0; $i < count($_layoutParams['menu']); $i++): ?>
+                    <?php
+                        if($item && $_layoutParams['menu'][$i]['id'] == $item) {
+                            $_item_style = 'current';
+                        } else {
+                            $_item_style = '';
+                        }
+                    ?>
+                        <li id="<?php echo $_item_style; ?>"><a href="<?php echo $_layoutParams['menu'][$i]['enlace']; ?>"><?php echo $_layoutParams['menu'][$i]['titulo']; ?></a></li>
+                    <?php endfor; ?>
+                    <?php endif; ?>
+                    </ul>
+                </div>
+
+                <img src="<?php echo $_layoutParams['ruta_img'] ?>navi_right.jpg" alt="navi_right" />
+            <!--navigation div container end here-->
+            </div>
+        </div>
