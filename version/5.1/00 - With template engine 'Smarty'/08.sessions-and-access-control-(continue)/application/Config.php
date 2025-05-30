a707e232f7e6c93de71120477369a93773ac6948
Brayan Esteves <brayan.esteves93@gmail.com>
2025-05-30 11:22:17 -0400

    (With template engine 'Smarty') 08 - Session & Access Control (Continue) [PHP version 5.1] [30/05/2025]

---
+++ version/5.1/00 - With template engine 'Smarty'/08.sessions-and-access-control-(continue)/application/Config.php
@@ -0,0 +1,34 @@
+<?php
+    define('BASE_URL', 'http://localhost/mvc/');
+    /**
+     * [SPANISH]:
+     * Esta constante significará el controlador por defecto de la aplicación.
+     * En caso de que no se envíe ningún controlador en la URL, solicitará el controlador 'index'.
+     * Y si queremos cambiar un valor por defecto, cambiamos los valores 'index'.
+     * ==============
+     * [ENGLISH]:
+     * This constant will mean the default controller of the application
+     * In case no controller is sent in the URL, it will request the 'index' controller
+     * And if we want to change a default value, we change the 'index' values.
+     */
+    define('DEFAULT_CONTROLLER', 'index');
+    /**
+     * [SPANISH]:
+     * La carpeta 'layout' que contiene el directorio 'views'.
+     * ==============
+     * [ENGLISH]:
+     * The folder 'layout'  that directory 'views'.
+     */
+    define('DEFAULT_LAYOUT', 'default');
+
+    define('APP_NAME', 'Mi Framework');
+    define('APP_SLOGAN', 'mi primer framework php y mvc...');
+    define('APP_COMPANY', 'www.dlancedu.com');
+    define('SESSION_TIME', 10);
+
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', '');
+    define('DB_NAME', 'mvc');
+    define('DB_CHAR', 'utf8');
+?>
