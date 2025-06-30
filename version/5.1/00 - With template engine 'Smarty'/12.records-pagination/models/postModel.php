8b3ad36d170e01204b593212f4d5910df28d5de3
Brayan Esteves <brayan.esteves93@gmail.com>
2025-06-29 22:51:33 -0400

    (With template engine 'Smarty') 12 - Records Pagination & Enviroment 'prueba' [PHP version 5.1] [29/06/2025]

---
+++ version/5.1/00 - With template engine 'Smarty'/12.records-pagination/models/postModel.php
@@ -0,0 +1,46 @@
+<?php
+    class postModel extends Model {
+        public function __construct() {
+            parent::__construct();
+        }
+
+        public function getPosts() {
+            $post = $this->_db->query("SELECT * FROM posts;");
+            return $post->fetchall();
+        }
+
+        public function getPost($id) {
+            $id = (int) $id;
+            $post = $this->_db->query("SELECT * FROM posts WHERE id = $id;");
+            return $post->fetch();
+        }
+
+        public function insertarPost($titulo, $cuerpo) {
+            $this->_db->prepare("INSERT INTO `posts` VALUES(null, :titulo, :cuerpo);")->execute(array(':titulo' => $titulo, ':cuerpo' => $cuerpo));
+        }
+
+        public function editarPost($id, $titulo, $cuerpo) {
+            $id = (int) $id;
+            $this->_db->prepare("UPDATE `posts` SET `titulo` = :titulo, `cuerpo` = :cuerpo) WHERE `id` = :id;")->execute(array(':id' => $id, ':titulo' => $titulo, ':cuerpo' => $cuerpo));
+        }
+
+        public function eliminarPost($id) {
+            $id = (int) $id;
+            $this->_db->prepare("DELETE FROM `posts` WHERE `id` = :id;")->execute(array(':id' => $id));
+        }
+
+        public function getPostsMock() {
+            $post = array(
+                'id' => 1,
+                'titulo' => 'Titulo Post',
+                'cuerpo' => 'Cuerpo Post...'
+            );
+
+            return $post;
+        }
+
+        public function insertarPrueba($nombre) {
+            $this->_db->prepare("INSERT INTO `prueba` VALUES(null, :nombre);")->execute(array(':nombre' => $nombre));
+        }
+    }
+?>
